05fc1df1a73e717163d5ba51f61e79d4c1720a6c
Nguyễn Ngọc Long <51698746+ZNLong2203@users.noreply.github.com>
2024-07-10 01:53:42 +0700

    add airflow

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM ubuntu:latest
-LABEL authors="Zkare"
-
-ENTRYPOINT ["top", "-b"]
+FROM apache/airflow:2.5.1
+COPY requirements.txt ./requirements.txt
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+COPY ./dags /opt/airflow/dags
+COPY ./src /opt/airflow/src
---
+++ dags/airflow.py
@@ -0,0 +1,117 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+import psycopg2
+from cyvcf2 import VCF
+
+
+# Define the function to create tables
+def create_table(conn, cur):
+    cur.execute("""        
+          CREATE TABLE IF NOT EXISTS variant(
+          id TEXT PRIMARY KEY,
+          name VARCHAR(255),
+          chrom VARCHAR(255),
+          pos VARCHAR(255),
+          ref VARCHAR(255),
+          alt VARCHAR(255),
+          qual FLOAT,
+          filter VARCHAR(255),
+          info JSONB
+      );
+
+      CREATE TABLE IF NOT EXISTS sample(
+          id TEXT PRIMARY KEY,
+          name VARCHAR(255) 
+      );
+
+      CREATE TABLE IF NOT EXISTS format(
+          id serial PRIMARY KEY,
+          variant_id TEXT,
+          sample_id TEXT,
+          allelic_depth TEXT,
+          allele_frequency FLOAT,
+          genotype VARCHAR(255),
+          FOREIGN KEY(variant_id) REFERENCES variant(id),
+          FOREIGN KEY(sample_id) REFERENCES sample(id)
+      );
+      """)
+    conn.commit()
+
+
+# Define the function to insert variants
+def insert_variant(conn, cur, vcf_file):
+    vcf = VCF(vcf_file)
+    for variant in vcf:
+        chrom, pos, ref, alt = variant.CHROM, variant.POS, variant.REF, variant.ALT
+        qual, filter = variant.QUAL, variant.FILTER
+        if (variant.ID == None):
+            cur.execute("""
+                  SELECT COUNT(*) FROM variant;
+              """)
+            variant.ID = str(cur.fetchone()[0] + 1)
+
+        cur.execute("""
+              INSERT INTO variant(id, name, chrom, pos, ref, alt, qual, filter)
+              VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
+          """, (variant.ID, variant.ID, chrom, pos, ref, alt, qual, filter))
+        conn.commit()
+
+        for i, sample in enumerate(vcf.samples):
+            cur.execute("""
+                              INSERT INTO sample(id, name)
+                              VALUES(%s, %s)
+                          """, (sample, sample))
+            conn.commit()
+
+            ad_values = ','.join(map(str, variant.format('AD')[i])).replace(',', ' ')
+            af_values = ','.join(map(str, variant.format('AF')[i]))
+            gt_values = variant.genotypes[0][i]
+            print(ad_values, af_values, gt_values)
+            cur.execute("""
+                  INSERT INTO format(variant_id, sample_id, allelic_depth, allele_frequency, genotype)
+                  VALUES(%s, %s, %s, %s, %s)
+                  """, (variant.ID, sample, ad_values, af_values, gt_values))
+            conn.commit()
+
+
+# Define default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Define the DAG
+dag = DAG(
+    'my_insert_data_dag',
+    default_args=default_args,
+    description='Insert data from VCF file into PostgreSQL',
+    schedule_interval=timedelta(days=1),
+)
+
+# Define tasks in the DAG using PythonOperator
+task_create_table = PythonOperator(
+    task_id='create_table_task',
+    python_callable=create_table,
+    op_kwargs={'conn': psycopg2.connect(host="postgres", database="vcf", user="postgres", password="test"),
+               'cur': psycopg2.connect(host="postgres", database="vcf", user="postgres", password="test").cursor()},
+    dag=dag,
+)
+
+task_insert_variant = PythonOperator(
+    task_id='insert_variant_task',
+    python_callable=insert_variant,
+    op_kwargs={'conn': psycopg2.connect(host="postgres", database="vcf", user="postgres", password="test"),
+               'cur': psycopg2.connect(host="postgres", database="vcf", user="postgres", password="test").cursor(),
+               'vcf_file': 'test.vep.vcf'},
+    dag=dag,
+)
+
+# Set task dependencies
+task_create_table >> task_insert_variant
+
